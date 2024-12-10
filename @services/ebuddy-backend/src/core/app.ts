647fbdc148f00cbc2c82e43df84038a8929e0034
Reja <rezanurrochmat@gmail.com>
2024-12-10 14:09:00 +0700

    (feat): setup cors

--- @services/ebuddy-backend/src/core/app.ts
+++ @services/ebuddy-backend/src/core/app.ts
@@ -1,11 +1,13 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import userRouter from "../routes/user.routes";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 app.use(express.json());
+app.use(cors())
 // User Router
 app.use(userRouter);
 
